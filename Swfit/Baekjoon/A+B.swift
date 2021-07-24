c4054d3f721e9081a1119784785528e4939dea2d
2unbini <75126613+2unbini@users.noreply.github.com>
2021-07-24 10:54:08 +0900

    1000

--- Swfit/Baekjoon/A+B.swift
+++ Swfit/Baekjoon/A+B.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-var value = readLine()!.components(separatedBy: " ").map { Int($0)! }
+var value = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
 var result = value[0] + value[1]
 print(result)
