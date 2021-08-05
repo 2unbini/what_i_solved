a5bc093431076408679b1855636cd0a179cebc4f
2unbini <75126613+2unbini@users.noreply.github.com>
2021-08-05 18:08:44 +0900

    Update 2xn타일링2.swift

--- Swfit/Baekjoon/2xn타일링2.swift
+++ Swfit/Baekjoon/2xn타일링2.swift
@@ -18,3 +18,26 @@ func solution() {
 }
 
 solution()
+
+// DP recursive function
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 1001)
+
+func makeSquare(_ i: Int) -> Int {
+    if i == 1 {
+        return 1
+    }
+    if i == 2 {
+        return 3
+    }
+    if dp[i] != 0 {
+        return dp[i]
+    }
+    
+    dp[i] = (makeSquare(i - 1) + (2 * makeSquare(i - 2))) % 10007
+    
+    return dp[i]
+}
+
+print(makeSquare(n))
