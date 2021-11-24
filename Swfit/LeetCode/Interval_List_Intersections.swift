0d9804de8163594cec00ae8b492e1488fd8f7920
Eunbin Kwon <75126613+2unbini@users.noreply.github.com>
2021-11-25 00:40:30 +0900

    Update Interval_List_Intersections.swift

--- Swfit/LeetCode/Interval_List_Intersections.swift
+++ Swfit/LeetCode/Interval_List_Intersections.swift
@@ -27,3 +27,34 @@ class Solution {
         return answer
     }
 }
+
+// Another Solution
+class Solution {
+    func intervalIntersection(_ firstList: [[Int]], _ secondList: [[Int]]) -> [[Int]] {
+        var answer = [[Int]]()
+        var fIndex = 0
+        var sIndex = 0
+        
+        if firstList.isEmpty || secondList.isEmpty {
+            return answer
+        }
+        
+        while fIndex < firstList.count && sIndex < secondList.count {
+            let startPoint = max(firstList[fIndex][0], secondList[sIndex][0])
+            let endPoint = min(firstList[fIndex][1], secondList[sIndex][1])
+            
+            if startPoint <= endPoint {
+                answer.append([startPoint, endPoint])
+            }
+            
+            if firstList[fIndex][1] < secondList[sIndex][1] {
+                fIndex += 1
+            }
+            else {
+                sIndex += 1
+            }
+        }
+        
+        return answer
+    }
+}
